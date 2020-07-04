024329d2d91d13cd3594e84341a77a14a9a3ddc5
Tyler Wheaton <xyzentw@gmail.com>
2020-07-04 17:23:11 -0500

    Exam update

--- Exam1/Exam1.py
+++ Exam1/Exam1.py
@@ -2,74 +2,34 @@
 import sys
 import time
 import serial
-import urllib.request as urllib2,json
 import RPi.GPIO as GPIO
-from Adafruit_IO import RequestError, Client, Feed, Data
 
-#Thingspeak API data
-READ_API_KEY='YP2XMIJDWOTSTWW4'
-CHANNEL_ID=1090663
-
-#Adafruit IO Dashboard data
-ADAFRUIT_IO_USERNAME = "xyzen"
-ADAFRUIT_IO_KEY = "aio_wdFQ059pSWIVwkXcakJtxnaEz25z"
-aio = Client(ADAFRUIT_IO_USERNAME, ADAFRUIT_IO_KEY)
+GPIO.setmode(GPIO.BOARD)
+outputs = [3, 5, 7, 8, 10, 11, 12, 13, 15, 16, 18, 19, 21, 22, 23, 24]
+pairs = [(3, 7), (5, 8), (10, 12), (11, 13), (15, 18), (16, 19), (21, 23), (22, 24)]
+[GPIO.setup(pin, GPIO.OUT) for pin in outputs]
+[GPIO.output(pin, GPIO.LOW) for pin in outputs]
 
 #Assignment Variables
-ser = serial.Serial("/dev/ttyACM1",9600) #Connect to serial
+ser = serial.Serial("/dev/ttyACM0",9600) #Connect to serial
 ser.baudrate = 9600 #Set baudrate
 
-#Data/Alarms arrays
-data = []
-alarms = []
-leds = [17,18,19,20,21,22]
-
-#Raspberry Pi GPIO setup
-GPIO.setmode(GPIO.BCM)
-GPIO.setwarnings(False)
-for x in leds()
-    GPIO.setup(x,GPIO.OUT)
-
-#Send data function, sends to Adafruit IO Dashboard
-def send_data(data):
-    feeds = aio.feeds() #Sets feeds from AIO
-    
-    for x in range(len(data)):
-        aio.send_data(feeds[x].key, data[x]) #Send data to AIO
-    
-def pull_data():
-    read_ser = ser.readline() #Read line
-    package = read_ser.decode('ASCII') #Decode ASCII characters
-    data = package.split('|') #Split package on '|'
-    return data #Return the data array
-    
 def check_alarms(data):
-    alarms = []
-    sensThres = {'Temperature':27, 'Humidity':41.9, 'Pressure':978.5,
-                 'Light':400, 'Dust':3805, 'Carbon Dioxide':4.1,
-                 'Carbon Monoxide':8.1, 'Distance':22}
-    x = 0
-    for sens in(sensThres):
-        if data[x] > sensThres[sens]
-            alarms.append(x)
-        x += 1
-    return alarms
+    sensThres = [27, 41.9, 978.5, 400, 3805, 4.1, 8.1, 22]
+    for i in range(8):
+        if data[i] > sensThres[i]:
+            GPIO.output(pairs[i][0], GPIO.HIGH)
+            GPIO.output(pairs[i][1], GPIO.LOW)
+        else:
+            GPIO.output(pairs[i][0], GPIO.LOW)
+            GPIO.output(pairs[i][1], GPIO.HIGH)
 
-def set_alarming(alarms,leds):
-    for loops in range(5)
-        for x in alarms:
-            GPIO.output(leds[x],GPIO.HIGH)
-        time.sleep(1)
-        for x in alarms:
-            GPIO.output(leds[x],GPIO.LOW)             
-        
-def main():
-    data = pull_data() #Calls pull_data function
-    alarms = check_alarms(data) #Calls the check_alarms function
-    if len(alarms) > 0:
-        set_alarming(alarms)
-    send_data(data) #Calls send data function
-    
-        
-if __name__ == '__main__': #To initiate main function
-    main()
+while True:
+    read_ser = ser.readline()
+    print(read_ser)
+    package = read_ser.decode('ASCII')
+    if package[:3] == "MSG":
+        data = package[3:].split("|")
+        data = [float(reading) for reading in data]
+        print(data)
+    	check_alarms(data)
